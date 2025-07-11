0bc535d718c6dfb47296874d4be82b1ac08d8ca2
Dmytro Astakhov <astahov.dmitriy.a@gmail.com>
2025-07-11 22:22:44 +0300

    Added fetching product cards from backend

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
+import axios from 'axios'
+
 import HeaderMenu from '@/components/HeaderMenu.vue'
 import CardList from '@/components/CardList.vue'
-import SideDrawer from '@/components/SideDrawer.vue'
+// import SideDrawer from '@/components/SideDrawer.vue'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: '',
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy,
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(`https://015b6f158224e20f.mokky.dev/items`, {
+      params,
+    })
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -13,15 +53,19 @@ import SideDrawer from '@/components/SideDrawer.vue'
         <h2 class="text-3xl font-bold mb-8">Всі кросівки</h2>
 
         <div class="flex gap-4">
-          <select class="py-2 px-3 border border-gray-300 rounded-md outline-none">
-            <option>По назві</option>
-            <option>По ціні ⬆️</option>
-            <option>По ціні ⬇️</option>
+          <select
+            @change="onChangeSelect"
+            class="py-2 px-3 border border-gray-300 rounded-md outline-none"
+          >
+            <option value="name">По назві</option>
+            <option value="price">По ціні ⬆️</option>
+            <option value="-price">По ціні ⬇️</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-3 top-3" src="/search.svg" alt="" />
             <input
+              @input="onChangeSearchInput"
               class="border border-gray-300 rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-500"
               type="text"
               placeholder="Пошук..."
@@ -30,7 +74,9 @@ import SideDrawer from '@/components/SideDrawer.vue'
         </div>
       </div>
 
-      <card-list />
+      <div class="mt-10">
+        <card-list :items="items" />
+      </div>
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,15 +1,19 @@
 <script setup>
 import ProductCard from './ProductCard.vue'
+
+defineProps({
+  items: Array,
+})
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
     <product-card
-      imageUrl="public/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="5000"
-      :isAdded="false"
-      :isFavorite="false"
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
     />
   </div>
 </template>
