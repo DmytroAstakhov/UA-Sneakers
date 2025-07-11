e0ae0c9934c9e3cec520965aa7e4f908e04a3502
Dmytro Astakhov <astahov.dmitriy.a@gmail.com>
2025-07-11 11:58:38 +0300

    Created header and product card

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
-<script setup></script>
+<script setup>
+import HeaderMenu from '@/components/HeaderMenu.vue'
+import ProductCard from '@/components/ProductCard.vue'
+</script>
 
-<template></template>
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+    <header-menu />
+    <product-card />
+  </div>
+</template>
